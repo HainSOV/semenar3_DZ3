03fac0e2fe63cbb139c93bdd73ae67fe2b918343
HainSOV <ya@hsov.ru>
2023-07-20 15:20:54 +0600

    KL

---
+++ ZD1.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+string text = Console.ReadLine();
+char[] reverseText = text.ToCharArray();
+Array.Reverse(reverseText);
+string finalText = new string(reverseText);
+if(text == finalText)
+{
+    Console.WriteLine("палидром");
+}
+else
+{
+    Console.WriteLine("не палиндром");
+}
